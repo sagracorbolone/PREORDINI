69e040084ab0d8bd43db916d6c3cf3bd4fb2ec4c
sagracorbolone <sagracorbolone@gmail.com>
2025-09-20 10:24:08 +0200

    20250920-SERATA PESCE-06

---
+++ .SyncFolders History/2025-08-23 19.28.16/preordini_lib/util/data.js
@@ -0,0 +1,248 @@
+// data.js - Generato automaticamente (Data: 2025-08-23T19:23:45.500593)
+
+var elencoPrincipale = [
+  "BEVANDE",
+  "CONTORNI E VARIE",
+  "MENU",
+  "PIATTO UNICO E PRIMI",
+  "SECONDI PIATTI"
+];
+var categorie = [
+  {
+    "id": 5,
+    "descrizione": "BEVANDE"
+  },
+  {
+    "id": 4,
+    "descrizione": "CONTORNI E VARIE"
+  },
+  {
+    "id": 6,
+    "descrizione": "MENU"
+  },
+  {
+    "id": 1,
+    "descrizione": "PIATTO UNICO E PRIMI"
+  },
+  {
+    "id": 3,
+    "descrizione": "SECONDI PIATTI"
+  }
+];
+var elencoPietanze = {
+  "BEVANDE": [
+    {
+      "id": 29,
+      "descrizione": "ACQUA FRIZZ. 0,50L",
+      "prezzo": "1"
+    },
+    {
+      "id": 28,
+      "descrizione": "ACQUA NAT. 0,50L",
+      "prezzo": "1"
+    },
+    {
+      "id": 37,
+      "descrizione": "ARANCIATA LATTINA 0,33L",
+      "prezzo": "2.5"
+    },
+    {
+      "id": 36,
+      "descrizione": "BICCH. BIRRA 0,40L",
+      "prezzo": "4.5"
+    },
+    {
+      "id": 31,
+      "descrizione": "BICCH. VINO BIANCO",
+      "prezzo": "1"
+    },
+    {
+      "id": 30,
+      "descrizione": "BICCH. VINO ROSSO",
+      "prezzo": "1"
+    },
+    {
+      "id": 40,
+      "descrizione": "BOTT.BIANCO SELEZ.",
+      "prezzo": "8"
+    },
+    {
+      "id": 41,
+      "descrizione": "BOTT.PROSECCO DOC",
+      "prezzo": "10"
+    },
+    {
+      "id": 39,
+      "descrizione": "BOTT.ROSSO SELEZ",
+      "prezzo": "8"
+    },
+    {
+      "id": 33,
+      "descrizione": "CARAFFA BIANCO 0,5L",
+      "prezzo": "3.5"
+    },
+    {
+      "id": 35,
+      "descrizione": "CARAFFA BIANCO 1L",
+      "prezzo": "6"
+    },
+    {
+      "id": 32,
+      "descrizione": "CARAFFA ROSSO 0,5L",
+      "prezzo": "3.5"
+    },
+    {
+      "id": 34,
+      "descrizione": "CARAFFA ROSSO 1L",
+      "prezzo": "6"
+    },
+    {
+      "id": 38,
+      "descrizione": "COCA-COLA LATTINA 0,33L",
+      "prezzo": "2.5"
+    },
+    {
+      "id": 47,
+      "descrizione": "THE LATTINA 0,33L",
+      "prezzo": "2.5"
+    }
+  ],
+  "CONTORNI E VARIE": [
+    {
+      "id": 25,
+      "descrizione": "FAGIOLI",
+      "prezzo": "3"
+    },
+    {
+      "id": 24,
+      "descrizione": "FUNGHI",
+      "prezzo": "5.5"
+    },
+    {
+      "id": 23,
+      "descrizione": "PATATINE",
+      "prezzo": "3"
+    },
+    {
+      "id": 26,
+      "descrizione": "SUPPL. PANE",
+      "prezzo": "0.5"
+    },
+    {
+      "id": 27,
+      "descrizione": "SUPPL.POLENTA",
+      "prezzo": "0.5"
+    }
+  ],
+  "MENU": [
+    {
+      "id": 43,
+      "descrizione": "MENU 2",
+      "prezzo": "10"
+    }
+  ],
+  "PIATTO UNICO E PRIMI": [
+    {
+      "id": 4,
+      "descrizione": "GNOCCHI ANATRA",
+      "prezzo": "6.5"
+    },
+    {
+      "id": 3,
+      "descrizione": "GNOCCHI RAGU'",
+      "prezzo": "6.5"
+    },
+    {
+      "id": 2,
+      "descrizione": "PASTA CINGHIALE",
+      "prezzo": "8"
+    }
+  ],
+  "SECONDI PIATTI": [
+    {
+      "id": 15,
+      "descrizione": "BACCALA'",
+      "prezzo": "12"
+    },
+    {
+      "id": 16,
+      "descrizione": "CALAMARI",
+      "prezzo": "10.5"
+    },
+    {
+      "id": 13,
+      "descrizione": "CAPRIOLO E FUNGHI",
+      "prezzo": "13"
+    },
+    {
+      "id": 6,
+      "descrizione": "COSTA",
+      "prezzo": "8.5"
+    },
+    {
+      "id": 9,
+      "descrizione": "COSTATA",
+      "prezzo": "18"
+    },
+    {
+      "id": 10,
+      "descrizione": "COSTATA BEN COTTA",
+      "prezzo": "18"
+    },
+    {
+      "id": 19,
+      "descrizione": "FORMAGGIO",
+      "prezzo": "5.5"
+    },
+    {
+      "id": 20,
+      "descrizione": "FORMAGGIO COTTO",
+      "prezzo": "7"
+    },
+    {
+      "id": 18,
+      "descrizione": "FRITTO MISTO",
+      "prezzo": "12.5"
+    },
+    {
+      "id": 22,
+      "descrizione": "GALLETTO ALLA BRACE",
+      "prezzo": "10"
+    },
+    {
+      "id": 8,
+      "descrizione": "MISTO",
+      "prezzo": "8.5"
+    },
+    {
+      "id": 11,
+      "descrizione": "PETTO DI POLLO",
+      "prezzo": "8.5"
+    },
+    {
+      "id": 7,
+      "descrizione": "SALSICCIA",
+      "prezzo": "8.5"
+    },
+    {
+      "id": 14,
+      "descrizione": "STINCO",
+      "prezzo": "12"
+    },
+    {
+      "id": 21,
+      "descrizione": "STRATAGLIATA",
+      "prezzo": "14"
+    },
+    {
+      "id": 12,
+      "descrizione": "TRIPPE",
+      "prezzo": "8"
+    }
+  ]
+};
+
+function Data(){
+  // ...
+}
+var dataManager = new Data();
